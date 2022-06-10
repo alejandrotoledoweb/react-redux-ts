24985d84aee9778a098823b59373612611ce7c92
Alejandro Toledo <atoledofr@gmail.com>
2022-06-10 08:55:49 -0500

    reorginize folders, files and add json-server dependency

---
+++ src/components/App/App.test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+test("renders learn react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/React Redux TypeScript App/i);
+  expect(linkElement).toBeVisible();
+});
---
+++ src/components/App/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return <div>React Redux TypeScript App</div>;
+}
+
+export default App;
