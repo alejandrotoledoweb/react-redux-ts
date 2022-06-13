bb998ff03e3555f6d99d7dd123c6802ec3aa3ad1
Alejandro Toledo <atoledofr@gmail.com>
2022-06-13 17:32:50 -0500

    change the createStore for legacy_createStore to delete the warning

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,4 @@
-import { combineReducers, createStore } from "redux";
+import { combineReducers, legacy_createStore as createStore } from "redux";
 import userEventsReducer from "./user-events";
 
 const rootReducer = combineReducers({
