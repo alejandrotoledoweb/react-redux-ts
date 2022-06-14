71cbef94cbee4654fa604b2d3ee490d24389d0fc
Alejandro Toledo <altoledo@pichincha.com>
2022-06-14 14:13:25 -0500

    change import paths for some files

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,9 @@ import App from "./components/App/App";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <Provider store={store}>
     <App />
-  </Provider>
+  </Provider>,
 );
